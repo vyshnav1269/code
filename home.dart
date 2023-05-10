6fe4e3d9c441d8fca628b1cb2b7555e3d3fd8bfe
vyshnav1269 <132980459+vyshnav1269@users.noreply.github.com>
2023-05-10 14:39:36 +0530

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../loginhere/loginpage.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -12,6 +14,78 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+     actions: [
+       PopupMenuButton(
+         elevation: 50,
+         shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.all(Radius.circular(10)),
+         ),
+           itemBuilder: (context) => [
+         PopupMenuItem(child: Column(
+           children: [
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: ListTile(
+                 leading: Icon(Icons.home),
+                 title: Text('Home',
+                 style: TextStyle(
+                   color: Colors.grey
+                 ),),
+               ),
+             ),
+             Divider()
+           ],
+         )),
+         PopupMenuItem(child: Column(
+           children: [
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: ListTile(
+                 leading: Icon(Icons.chat_sharp),
+                 title: Text('chats',
+                 style: TextStyle(
+
+                   color: Colors.grey
+                 ),),
+               ),
+             ),
+             Divider()
+           ],
+         )),
+             PopupMenuItem(child: Column(
+               children: [
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: ListTile(
+                     leading: Icon(Icons.bar_chart),
+                     title: Text('statistics',
+                     style:TextStyle(
+                       color: Colors.grey
+                     ) ,),
+                   ),
+                 ),
+                 Divider()
+               ],
+             )),
+             PopupMenuItem(child: Column(
+               children: [
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: ListTile(
+                     onTap: () {
+                       Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(),));
+                     },
+                     leading: Icon(Icons.logout),
+                     title: Text('Sign out',
+                     style: TextStyle(
+                       color: Colors.grey
+                     ),),
+                   ),
+                 )
+               ],
+             )),
+       ]),
+     ],
       ),
     );
   }
